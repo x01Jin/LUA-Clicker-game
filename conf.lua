6e8008acdda87892b221dd9b1c29f14d7a2be613
Jin <x01Jin@users.noreply.github.com>
2024-03-07 15:46:31 +0800

    updated release

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+    t.window.title = "Clicker game ni bogart"
+    t.window.width = 800
+    t.window.height = 600
+    t.window.borderless = false
+    t.window.resizable = true
+    t.window.minwidth = 800
+    t.window.minheight = 600
+end
